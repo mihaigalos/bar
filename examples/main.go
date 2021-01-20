1312be71fb916f93c1c00146c31698cc3445266d
Mihai Galos <mihaigalos@gmail.com>
2021-01-20 21:14:18 +0100

    Use repo name as package

--- examples/main.go
+++ examples/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"time"
+
+	"github.com/mihaigalos/bar"
 )
 
 func main() {
-	var bar Bar
+	var bar bar.Bar
 	bar.New(0, 100)
 	for i := 0; i <= 100; i++ {
 		time.Sleep(100 * time.Millisecond)
